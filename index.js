d12b3261bd107af6d5a2c001de21ae80246e8eeb
Baher902 <Baheratef10@gmail.com>
2021-01-17 00:12:00 +0200

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+ let btn1 = document.getElementById('btn1')
+ let btn2 = document.getElementById('btn2')
+ let btn3 = document.getElementById('btn3')
+ let img = document.querySelector('#img') 
+
+ 
+ btn1.onclick = function btn1()
+ {
+     img.src="img/gg (1).jpg";
+ }
+ btn2.onclick = function btn2()
+ {
+    img.src="img/gg (2).jpg";
+
+ }
+
+ btn3.onclick = function btn3()
+ {
+    img.src="img/gg (3).jpg";
+
+ }
